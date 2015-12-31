450a0e2eb288eb4f8fb3ac3b08fb08ee35ab3e74
Tim 'mithro' Ansell <mithro@mithis.com>
2015-12-31 16:30:03 +1100

    Flash script will wait for the microchip device to appear.

--- firmware/flash.sh
+++ firmware/flash.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-./mphidflash/binaries/mphidflash-1.6-linux-64 -reset -write "./tofe-lowspeedio.X/dist/default/production/tofe-lowspeedio.X.production.hex"
+while true; do
+	if lsusb | grep -q "Microchip"; then
+		echo "Should flash"
+		./mphidflash/binaries/mphidflash-1.6-linux-64 -reset -write "./tofe-lowspeedio.X/dist/default/production/tofe-lowspeedio.X.production.hex"
+	else
+		echo "No device..."
+	fi
+	sleep 1
+done
