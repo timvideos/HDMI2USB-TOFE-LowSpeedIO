0b22e74a59c1ec77e4ab2a76378f84f5d08fe578
Tim 'mithro' Ansell <mithro@mithis.com>
2016-01-06 18:22:09 +1100

    Make flash script more strict.

--- firmware/flash.sh
+++ firmware/flash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-	if lsusb | grep -q "Microchip"; then
+	if lsusb | grep -q "04d8:003c"; then
 		echo "Should flash"
 		./mphidflash/binaries/mphidflash-1.6-linux-64 -reset -write "./tofe-lowspeedio.X/dist/default/production/tofe-lowspeedio.X.production.hex"
 	else
